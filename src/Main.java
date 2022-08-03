d6e2510632417a38ad980f9a0d673b27147e059a
emukhina <emukhina24@gmail.com>
2022-08-03 22:38:30 +0300

    расчет миль

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTiket = 100;
+        int bonus = 20;
+        int sumBonus = priceTiket / bonus;
+
+        System.out.println("Начисленные мили " + sumBonus);
+
+
+    }
+}
